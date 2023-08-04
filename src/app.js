2da2f1b123b3c20300463ef3369e98f0bbbb2d3b
Genesis Longart <102156923+GenyLongart@users.noreply.github.com>
2023-08-04 21:22:01 +0000

    Funcionalidad de contenido aleatorio

--- src/app.js
+++ src/app.js
@@ -5,7 +5,41 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
+let emojis = ["♦", "♥", "♠", "♣"];
+let valores = [
+  "2",
+  "3",
+  "4",
+  "5",
+  "6",
+  "7",
+  "8",
+  "9",
+  "10",
+  "J",
+  "A",
+  "K",
+  "Q"
+];
+let colores = ["red", "black"];
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  const upperEmojiDiv = document.getElementById("upperEmoji");
+  const bottomEmojiDiv = document.getElementById("bottomEmoji");
+  const numLetDiv = document.getElementById("numLet");
+  // emoji aleatorio
+  const randomIndex = Math.floor(Math.random() * emojis.length);
+  const selectedEmoji = emojis[randomIndex];
+  //color aleatorio
+  const colorRandom = Math.floor(Math.random() * colores.length);
+  const selectedColor = colores[colorRandom];
+  // número aleatorio
+  const randomIndexValue = Math.floor(Math.random() * valores.length);
+  const selectedValue = valores[randomIndexValue];
+  // Agregando contenido y estilos a los divs
+  upperEmojiDiv.textContent = selectedEmoji;
+  bottomEmojiDiv.textContent = selectedEmoji;
+  upperEmojiDiv.style.color = selectedColor;
+  bottomEmojiDiv.style.color = selectedColor;
+  numLetDiv.style.color = selectedColor;
+  numLetDiv.textContent = selectedValue;
 };
